0eaa99b1db1dad7c15733338e481e0a4f4a23859
Antoine Demon-Chaine <a.demonchaine@gmail.com>
2020-05-22 11:23:59 +0200

    code cleaning

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -67,7 +67,7 @@ export const createPlace = async (req, res, next) => {
       new HttpError("Invalid inputs passed please check your data", 422)
     );
   }
-  const { title, description, address, creator } = req.body;
+  const { title, description, address } = req.body;
 
   let coordinates;
   try {
@@ -82,12 +82,12 @@ export const createPlace = async (req, res, next) => {
     address,
     location: coordinates,
     image: req.file.path,
-    creator,
+    creator: req.userData.userId,
   });
 
   let user;
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
   } catch (error) {
     return next(
       new HttpError(
